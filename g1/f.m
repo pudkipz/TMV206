ec5a4355a590eb703f88c466812e79714b56b911
Mat <jippie95@hotmail.com>
2019-02-13 10:52:19 +0100

    Update f.m
    La till lite dokumentation samt rubriker över subplottarna så att man ser vad som plottas i vilken subplot.
    
    /Matilda S.

--- g1/f.m
+++ g1/f.m
@@ -1,7 +1,9 @@
-function [] = f(A,b,X)
+function [] = f2(A,b,X)
     clf;
-    X=[X, X(:,1)];
+    X=[X, X(:,1)]; %copy the first point to the end to close the chain
     Axb = A*X +b;
+    
+    %calculate max and min values to find ideal plot size
     maxX = max(max(X(1,:), max(Axb(1,:))));
     maxY = max(max(X(2,:), max(Axb(2,:))));
     minX = min(min(X(1,:), min(Axb(1,:))));
@@ -9,15 +11,20 @@ function [] = f(A,b,X)
     MIN = min(minX, minY);
     MAX = max(maxX, maxY);
     padding = (MAX - MIN)/10;
+    
+    %plot X in subplot 1
     subplot(1,2,1)
     fill(X(1,:), X(2,:),'g')
     line([MIN-padding MAX+padding], [0 0],'color','k') %x-axis
     line( [0 0],[MIN-padding MAX+padding],'color','k') %x-axis
     axis([MIN-padding,MAX+padding, MIN-padding, MAX+padding])
+    title('X')
+    
+    %plot AX+b in subplot 2
     subplot(1,2,2);
     fill(Axb(1,:), Axb(2,:),'b')
     line([MIN-padding MAX+padding], [0 0],'color','k') %x-axis
     line( [0 0],[MIN-padding MAX+padding],'color','k') %x-axis
     axis([MIN-padding,MAX+padding, MIN-padding, MAX+padding])
+    title('AX + b')
 end
-
